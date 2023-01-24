592000c8e00609881fc8b5852ad49125d7ab30f0
unknown <nathaniel.farias92@gmail.com>
2023-01-24 10:21:16 -0300

    projeto finalizado e pronto para build

--- src/Conteudos/Final.js
+++ src/Conteudos/Final.js
@@ -1,11 +1,67 @@
-import styles from '../Conteudos/Final.module.css'
+import styles from "../Conteudos/Final.module.css";
 
-function Final(){
-    return <div className={styles.conteudo}>
+function Final() {
+  return (
+    <div className={styles.conteudo}>
+      <h1>
+        Veja tudo o que você vai <span>aprender!</span>
+      </h1>
 
-        <h1>Final</h1>
+      <div className={styles.containerUm}>
+        <div className={styles.caixa}>
+          <h3>Modulo 01</h3>
+          <p>Pronomes pessoais</p>
+          <p>Frases interrogativas</p>
+          <p>Adjetivos comuns e demonstrativos</p>
+          <p>Verbos auxiliares e de ação</p>
+          <p>Vocabulário básico de palavras</p>
+          <p>Verbo “to be”</p>
+          <p>Modo imperativo</p>
+          <p>Preposições de tempo e lugar</p>
+          <p>Como estruturar perguntas</p>
+          <p>Possessivos</p>
+          <p>Quantificadores</p>
+          <p>There to be</p>
+          <p>Expressões de tempo</p>
+          <p>Verbos modais</p>
+        </div>
 
+        <div className={styles.caixa}>
+          <h3>Modulo 02</h3>
+          <p>Presente simple</p>
+          <p>Presente contínuo</p>
+          <p>Passado simples</p>
+          <p>Passado contínuo</p>
+          <p>Futuro simples</p>
+          <p>Advérbios</p>
+          <p>Comparativoss</p>
+          <p>Superlativos</p>
+          <p>Futuro contínuo</p>
+          <p>Condicionais</p>
+          <p>Passado contínuo e perfeito</p>
+          <p>Presente perfeito contínuo</p>
+          <p>Voz passiva</p>
+          <p>Previsões</p>
+        </div>
+
+        <div className={styles.caixa}>
+          <h3>Modulo 03</h3>
+          <p>Pronomes relativos</p>
+          <p>Desejos</p>
+          <p>Condicionais mistas</p>
+          <p>Tempos verbais de narrativa</p>
+          <p>Futuro contínuo</p>
+          <p>Phrasal verbs</p>
+          <p>Vocabulário mais técnico</p>
+        </div>
+      </div>
+      <h2>E muito, muito mais!</h2>
+      <p>
+        Você terá acesso a um curso completo, 100% online e com aulas práticas
+        em <br></br>vídeo com tudo que precisa para se tornar um fluente em inglês.
+      </p>
     </div>
+  );
 }
 
 export default Final;
--- src/Conteudos/Meio.js
+++ src/Conteudos/Meio.js
@@ -1,11 +1,26 @@
-import styles from '../Conteudos/Meio.module.css'
+import styles from "../Conteudos/Meio.module.css";
+import foto01 from "../IMGS/professor.png";
 
-function Meio(){
-    return <div className={styles.conteudo}>
+function Meio() {
+  return (
+    <div className={styles.conteudo}>
+      <div className={styles.containerUm}>
+        <h1>Prof. Rui</h1>
+        <p>
+          Professor Rui é o professor de inglês mais <span>popular</span> da internet. Nativo
+          do Canadá, leciona no Brasil <span>há mais de 10 anos</span>, desenvolveu uma
+          metodologia <span>unica e revolucionária</span> que garante o aprendizado
+          <span>definitivo</span> do inglês. Foi professor de línguas estrangeiras em
+          <span>diversas</span> Universidades pelo Brasil. Hoje, possui em suas redes sociais
+          mais de <span>5 milhões</span> de seguidores e oferece a melhor <span>plataforma e metodologia</span> de
+          ensino de inglês do mercado.
+        </p>
 
-        <h1>Meio</h1>
+      </div>
 
+      <img src={foto01} alt="Professor Rui"></img>
     </div>
+  );
 }
 
 export default Meio;
